81a77f3dbb2e090fc0c2e2994ba98b22b807fe28
Z70007Z <Z-7000@yandex.ru>
2022-09-08 01:08:56 +0800

    R7

---
+++ element-search/popups/task.js
@@ -0,0 +1,18 @@
+
+
+const elemevtP = document.getElementById('modal_main');
+const elemevtS = document.getElementById('modal_success');
+modal_success
+elemevtP.classList.add("modal_active");
+
+addEventListener("click", function(event) {
+    let infoClassName = event.target.classList;
+
+    if (String(infoClassName).includes('modal__close')) {
+        elemevtP.classList.remove("modal_active"); 
+        elemevtS.classList.remove("modal_active"); 
+        if (String(infoClassName).includes('show-success')) {
+            elemevtS.classList.add("modal_active");
+        }   
+    }  
+});
