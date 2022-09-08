57d862c302fe4f564be42921210297a20406b037
Z70007Z <Z-7000@yandex.ru>
2022-09-08 23:58:02 +0800

    К74

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -14,3 +14,17 @@ for(let item of elementList) {
     
     });    
 }
+
+
+// [].forEach.call(elementList, function(elem) {
+//     elem.addEventListener("click", function(event) {  
+        
+//         const previousActive = elem.nextElementSibling;
+//         if (previousActive.className = "menu") {
+//             previousActive.classList.add('menu_active');
+//         }
+    
+//     });
+// });
+
+
