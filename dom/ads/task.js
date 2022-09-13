52bd70a6d28fcf00b584c2d5cad60f847a65fa28
Z70007Z <Z-7000@yandex.ru>
2022-09-13 23:48:18 +0800

    9_2

---
+++ dom/ads/task.js
@@ -0,0 +1,40 @@
+
+elemenActiveFirst =  Array.from(document.querySelectorAll('.rotator__case_active'));
+elemenSpan = Array.from(document.querySelectorAll('.rotator__case'));
+
+let Speed;
+
+for (let j=0; j < elemenActiveFirst.length; j++) { 
+    elemenActiveFirst[j].style.color = elemenActiveFirst[j].getAttribute('data-color');
+    Speed = elemenActiveFirst[j].getAttribute('data-speed');
+}
+
+function timerText() {
+    let Speed;
+    for (let i=0; i < elemenSpan.length; i++) {    
+        if (elemenSpan[i].classList.contains("rotator__case_active")) {
+            const elementNext = elemenSpan[i].nextElementSibling;
+            if (elementNext !== null) {
+                elemenSpan[i].classList.remove("rotator__case_active"); 
+                elementNext.style.color = elementNext.getAttribute('data-color');
+                Speed = elementNext.getAttribute('data-speed');
+                elementNext.classList.add("rotator__case_active");
+                i +=1;
+            } else {
+                elemenSpan[i].classList.remove("rotator__case_active");
+                const elemFirstChild = elemenSpan[i].closest('.rotator').firstElementChild;               
+                elemFirstChild.classList.add("rotator__case_active");
+                elemFirstChild.style.color = elemFirstChild.getAttribute('data-color');
+                Speed = elemFirstChild.getAttribute('data-speed');
+                if ((i + 1) > elemenSpan.length) {
+                    break;
+                } else {
+                    i +=1;  
+                }
+            }
+        }
+    }    
+
+}
+
+let timeoutId = window.setInterval(timerText, Speed);
