dc2eb7806234a3ae3bde2b60a5b5f08fc797ed37
Z70007Z <Z-7000@yandex.ru>
2022-09-14 12:16:18 +0800

    9_5

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,15 +1,16 @@
 
-const elemenReveal = document.querySelector('.reveal');
-
+const elemenRevealAll = document.querySelectorAll('.reveal');
 
 addEventListener('scroll', function(elem) {
-    const topReveal = elemenReveal.getBoundingClientRect().top;
-    const bottomReveal = elemenReveal.getBoundingClientRect().bottom;
-
-    if ((bottomReveal < window.innerHeight) && (topReveal > 0)) {
-        elemenReveal.classList.add("reveal_active");
-    } else {
-        elemenReveal.classList.remove("reveal_active");
+    for(let itemLink of elemenRevealAll) {
+        // const topReveal = itemLink.getBoundingClientRect().top;
+        // const bottomReveal = itemLink.getBoundingClientRect().bottom;   
+        const {top, bottom} = itemLink.getBoundingClientRect();     
+        if ((bottom < window.innerHeight) && (top > 0)) {
+            itemLink.classList.add("reveal_active");
+        } else {
+            itemLink.classList.remove("reveal_active");
+        } 
     }
-    
+
 });
