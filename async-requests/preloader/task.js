3b0128380b6b2a14c6d7e83468305ca499750eac
Z70007Z <Z-7000@yandex.ru>
2022-09-25 15:21:33 +0800

    12_1

---
+++ async-requests/preloader/task.js
@@ -0,0 +1,49 @@
+
+
+const elementItems  = document.getElementById('items');
+const elementImg = document.querySelector(".loader");
+const xhr = new XMLHttpRequest();
+
+//  localStorage.clear();
+
+function htmlAddCode(Atr,Sum) {
+    elementItems.innerHTML += `
+    <div class="item">
+        <div class="item__code">
+            ${Atr}
+        </div>
+        <div class="item__value">
+            ${Sum}            
+        </div>
+        <div class="item__currency">
+            руб.
+        </div>
+    </div>
+    `;
+}
+
+xhr.addEventListener('readystatechange',() => {
+    if (xhr.readyState === xhr.DONE) {
+        if (elementImg.classList.contains('loader_active')) {
+            elementImg.classList.remove('loader_active');
+
+            let object = JSON.parse(xhr.responseText);
+          
+            Object.entries(object.response.Valute).forEach((entry) => {
+                const [key, value] = entry;
+                const valueCharCode = value.CharCode;
+                const valueValue = value.Value;
+                    // console.log(localStorage[valueCharCode]);
+                if (localStorage[valueCharCode] !== undefined) {
+                    htmlAddCode(valueCharCode,localStorage[valueCharCode]);
+                } else {
+                    htmlAddCode(valueCharCode,"Пока нет значения!");
+                }
+                localStorage.setItem(valueCharCode, valueValue);               
+            });
+        }
+    }
+})
+
+xhr.open("GET", "https://netology-slow-rest.herokuapp.com");
+xhr.send();
