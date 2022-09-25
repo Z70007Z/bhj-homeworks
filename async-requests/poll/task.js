928ae8ca4e958184582e0ce4daa14ebe6099cf08
Z70007Z <Z-7000@yandex.ru>
2022-09-25 20:51:40 +0800

    12_3

---
+++ async-requests/poll/task.js
@@ -0,0 +1,67 @@
+
+const elementPollAnswers = document.querySelector(".poll__answers");
+const elementPollTitle = document.querySelector(".poll__title");
+let id;
+
+const xhr = new XMLHttpRequest();
+
+document.addEventListener('click', (event) => {
+
+    event.preventDefault();
+    if ((event.target.classList).contains("poll__answer")) {
+        alert("Спасибо, ваш голос засчитан!");
+        let answer;
+        xhr.open("POST", "https://netology-slow-rest.herokuapp.com/poll.php");
+        xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+
+        const elementPollAnswer = document.querySelectorAll(".poll__answer");
+        for (let i=0; i < elementPollAnswer.length; i++) {
+            if (elementPollAnswer.textContent = event.target.textContent) {
+                answer = i;
+            }
+            elementPollAnswer[i].remove();
+        }
+
+        xhr.send(`vote=${id}&answer=${answer}`);
+    }        
+}); 
+
+function htmlAddCode(answer) {
+    elementPollAnswers.innerHTML += `
+    <button class="poll__answer">
+        ${answer}
+    </button>
+    `;
+}
+
+function htmlAddCodeStat(key, answer) {
+    elementPollAnswers.innerHTML += `
+    <div class="poll__stat">
+       ${key} : ${answer} голосов!
+    </div>
+    `;
+}
+
+xhr.addEventListener('readystatechange',(event) => {
+    event.preventDefault();
+    if (xhr.readyState === xhr.DONE) {
+        let object = JSON.parse(xhr.responseText);
+
+        if (object.data !== undefined) {
+            id = object.id;
+            elementPollTitle.textContent = object.data.title;
+            Object.entries(object.data.answers).forEach((entry) => {
+                const [key, value] = entry;
+                htmlAddCode(value);                         
+            });
+        } else if (object.stat !== undefined) {
+            Object.entries(object.stat).forEach((entry) => {
+                const [key, value] = entry;
+                htmlAddCodeStat(value.answer, value.votes);                         
+            });           
+        }
+    }
+})
+
+xhr.open("GET", "https://netology-slow-rest.herokuapp.com/poll.php");
+xhr.send();
